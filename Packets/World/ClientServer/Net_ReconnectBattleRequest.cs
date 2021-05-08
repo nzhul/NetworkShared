51af9429de857883b6b79291a4876dd0dd934085
nzhul <dobromirivanov1@gmail.com>
2021-05-08 08:57:02 +0300

    refactor Net_ReconnectBattleRequest.cs

--- Packets/World/ClientServer/Net_ReconnectBattleRequest.cs
+++ Packets/World/ClientServer/Net_ReconnectBattleRequest.cs
@@ -1,17 +1,27 @@
 ﻿using System;
+using LiteNetLib.Utils;
 
 namespace NetworkingShared.Packets.World.ClientServer
 {
-    [Serializable]
-    public class Net_ReconnectBattleRequest : NetMessage
+    public class Net_ReconnectBattleRequest : INetPacket
     {
-        public Net_ReconnectBattleRequest()
-        {
-            OperationCode = NetOperationCode.ReconnectBattleRequest;
-        }
+        public PacketType Type => PacketType.ReconnectBattleRequest;
 
         public int GameId { get; set; } // TODO: later on the game id should be resolved by the server.
 
         public Guid BattleId { get; set; } // TODO: later on the battle id should be resolved by the server.
+
+        public void Deserialize(NetDataReader reader)
+        {
+            GameId = reader.GetInt();
+            BattleId = Guid.Parse(reader.GetString());
+        }
+
+        public void Serialize(NetDataWriter writer)
+        {
+            writer.Put((byte)Type);
+            writer.Put(GameId);
+            writer.Put(BattleId.ToString());
+        }
     }
 }
