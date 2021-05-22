e5d24a65ff5906d4dfb88c8191e0f050e75ad6fd
nzhul <dobromirivanov1@gmail.com>
2021-05-22 12:39:46 +0300

    add gameString prop to OnStartGame packet

--- Packets/World/ServerClient/Net_OnStartGame.cs
+++ Packets/World/ServerClient/Net_OnStartGame.cs
@@ -6,17 +6,21 @@ namespace NetworkingShared.Packets.World.ServerClient
     {
         public int GameId { get; set; }
 
+        public string GameString { get; set; }
+
         public PacketType Type => PacketType.OnStartGame;
 
         public void Deserialize(NetDataReader reader)
         {
             GameId = reader.GetInt();
+            GameString = reader.GetString();
         }
 
         public void Serialize(NetDataWriter writer)
         {
             writer.Put((byte)Type);
             writer.Put(GameId);
+            writer.Put(GameString);
         }
     }
 }
