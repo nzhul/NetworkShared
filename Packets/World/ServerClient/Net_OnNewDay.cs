e357a2f64e54ea1445f4def520e9655c07dc594e
nzhul <dobromirivanov1@gmail.com>
2021-07-01 08:53:14 +0300

    update Net_OnNewDay event to contain also day, week and month data

--- Packets/World/ServerClient/Net_OnNewDay.cs
+++ Packets/World/ServerClient/Net_OnNewDay.cs
@@ -7,17 +7,29 @@ namespace GameServer.NetworkShared.Packets.World.ServerClient
     {
         public PacketType Type => PacketType.OnNewDay;
 
-        public int CurrentDay { get; set; }
+        public int Day { get; set; }
+
+        public int Week { get; set; }
+
+        public int Month { get; set; }
+
+        public int TotalDays { get; set; }
 
         public void Deserialize(NetDataReader reader)
         {
-            CurrentDay = reader.GetInt();
+            Day = reader.GetInt();
+            Week = reader.GetInt();
+            Month = reader.GetInt();
+            TotalDays = reader.GetInt();
         }
 
         public void Serialize(NetDataWriter writer)
         {
             writer.Put((byte)Type);
-            writer.Put(CurrentDay);
+            writer.Put(Day);
+            writer.Put(Week);
+            writer.Put(Month);
+            writer.Put(TotalDays);
         }
     }
 }
