90266ce873b009ccb5a018cfa038aec66ba57fbd
nzhul <dobromirivanov1@gmail.com>
2021-07-11 16:28:13 +0300

    extend ArmyParams to contain SelectedUnitId field

--- Models/ArmyParams.cs
+++ Models/ArmyParams.cs
@@ -8,26 +8,31 @@ namespace GameServer.NetworkShared.Models
         {
         }
 
-        public ArmyParams(int id, int order)
+        public ArmyParams(int id, int order, int selectedUnitId)
         {
             Id = id;
             Order = order;
+            SelectedUnitId = selectedUnitId;
         }
 
         public int Id { get; set; }
 
         public int Order { get; set; }
 
+        public int SelectedUnitId { get; set; }
+
         public void Deserialize(NetDataReader reader)
         {
             Id = reader.GetInt();
             Order = reader.GetInt();
+            SelectedUnitId = reader.GetInt();
         }
 
         public void Serialize(NetDataWriter writer)
         {
             writer.Put(Id);
             writer.Put(Order);
+            writer.Put(SelectedUnitId);
         }
     }
 }
