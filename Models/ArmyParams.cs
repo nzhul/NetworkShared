df3cf82a5af09983395f3c8285a5f4ba1b7aa1e8
nzhul <dobromirivanov1@gmail.com>
2022-01-15 16:18:32 +0200

    change armyParams.cs to include owner and enemy selected unit ids

--- Models/ArmyParams.cs
+++ Models/ArmyParams.cs
@@ -8,31 +8,41 @@ namespace GameServer.NetworkShared.Models
         {
         }
 
-        public ArmyParams(int id, int order, int selectedUnitId)
+        public ArmyParams(int id, int order, int ownerSelectedUnitId, int enemySelectedUnitId)
         {
             Id = id;
             Order = order;
-            SelectedUnitId = selectedUnitId;
+            //SelectedUnitId = selectedUnitId;
+            OwnerSelectedUnitId = ownerSelectedUnitId;
+            EnemySelectedUnitId = enemySelectedUnitId;
         }
 
         public int Id { get; set; }
 
         public int Order { get; set; }
 
-        public int SelectedUnitId { get; set; }
+        //public int SelectedUnitId { get; set; }
+
+        public int OwnerSelectedUnitId { get; set; }
+
+        public int EnemySelectedUnitId { get; set; }
 
         public void Deserialize(NetDataReader reader)
         {
             Id = reader.GetInt();
             Order = reader.GetInt();
-            SelectedUnitId = reader.GetInt();
+            //SelectedUnitId = reader.GetInt();
+            OwnerSelectedUnitId = reader.GetInt();
+            EnemySelectedUnitId = reader.GetInt();
         }
 
         public void Serialize(NetDataWriter writer)
         {
             writer.Put(Id);
             writer.Put(Order);
-            writer.Put(SelectedUnitId);
+            //writer.Put(SelectedUnitId);
+            writer.Put(OwnerSelectedUnitId);
+            writer.Put(EnemySelectedUnitId);
         }
     }
 }
