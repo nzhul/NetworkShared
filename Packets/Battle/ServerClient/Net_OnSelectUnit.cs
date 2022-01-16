52404a1338e9987151438278d1e2776896b01486
nzhul <dobromirivanov1@gmail.com>
2022-01-16 16:54:04 +0200

    add ActorUserId to Net_OnSelectUnit packet

--- Packets/Battle/ServerClient/Net_OnSelectUnit.cs
+++ Packets/Battle/ServerClient/Net_OnSelectUnit.cs
@@ -7,16 +7,20 @@ namespace GameServer.NetworkShared.Packets.Battle.ServerClient
     {
         public PacketType Type => PacketType.OnSelectUnit;
 
+        public int ActorUserId { get; set; }
+
         public int SelectedUnitId { get; set; }
 
         public void Deserialize(NetDataReader reader)
         {
+            ActorUserId = reader.GetInt();
             SelectedUnitId = reader.GetInt();
         }
 
         public void Serialize(NetDataWriter writer)
         {
             writer.Put((byte)Type);
+            writer.Put(ActorUserId);
             writer.Put(SelectedUnitId);
         }
     }
