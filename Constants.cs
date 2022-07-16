0bc83c5c1876b641de5578eb4bc98a71caa18f48
nzhul <dobromirivanov1@gmail.com>
2022-07-16 19:08:05 +0300

    update constants

--- Constants.cs
+++ Constants.cs
@@ -4,7 +4,7 @@ namespace GameServer.NetworkShared
 {
     public static class Constants
     {
-        public readonly static TimeSpan BATTLE_TURN_DURATION = new TimeSpan(0, 0, 20);
+        public readonly static TimeSpan BATTLE_TURN_DURATION = new TimeSpan(0, 0, 30);
         public readonly static TimeSpan IDLE_TIMEOUT = new TimeSpan(0, 5, 50); // Use this for testing. Real one is bellow!
         //private const int IDLE_TIMEOUT = (TURN_DURATION * 2) + (TURN_DURATION / 2); // seconds -> 20 * 2 + 20 / 2 = 40 + 10 = 50
         public readonly static TimeSpan DAY_DURATION = new TimeSpan(0, 0, 20);
